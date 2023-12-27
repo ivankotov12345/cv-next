82855984b6d11a98be96fea3c8653539dbce13c8
Ivan Kotov <ivankotov12345@gmail.com>
2023-12-28 00:19:43 +0300

    fix: fix button and links for mobile devices

--- components/contacts/contacts.tsx
+++ components/contacts/contacts.tsx
@@ -12,10 +12,10 @@ export default function Contacts() {
           <th>Gmail:</th>
       </tr>
       <tr className={styles.table_row_info}>
-          <td><a href='https://t.me/muddy_buddy'>Jean Cat</a></td>
+          <td><a href='https://t.me/muddy_buddy' target='_blank'>Jean Cat</a></td>
           <td><a href='tel:+375295635835'>+375295635835</a></td>
-          <td><a href='skype:375295635835'>Ivan Kotov</a></td>
-          <td><a href='https://www.linkedin.com/in/ivan-kotov-7b1012238/'>Ivan Kotov</a></td>
+          <td><a href='skype:375295635835' target='_blank'>Ivan Kotov</a></td>
+          <td><a href='https://www.linkedin.com/in/ivan-kotov-7b1012238/' target='_blank'>Ivan Kotov</a></td>
           <td><a href='mailto:ivankotov12345@gmail.com'>ivankotov12345@gmail.com</a></td>
       </tr>
       </tbody>
